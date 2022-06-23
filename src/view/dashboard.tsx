95153345ff3b620375968414ed63c0b071a71e01
Eli250 <hirwaeli@outlook.com>
2022-06-23 18:11:11 +0200

    removed unused imports

--- src/view/dashboard.tsx
+++ src/view/dashboard.tsx
@@ -9,7 +9,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import MuiDrawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import Link from "@mui/material/Link";
 import List from "@mui/material/List";
 import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
